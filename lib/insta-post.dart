a863fdfe889f16b4d3804806793bf5ddffdfe254
Jensen098 <nickjensen098@gmail.com>
2021-02-11 18:12:53 +1000

    Create insta-post.dart

--- lib/insta-post.dart
+++ lib/insta-post.dart
@@ -3,6 +3,36 @@ import 'package:flutter/material.dart';
 class InstaPost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildProfileUsername(),
+              Icon(Icons.more_horiz),
+            ],
+          ),
+        ),
+        Image.asset('assets/chaseemmons_post.jpg'),
+      ],
+    );
+  }
+
+  Row _buildProfileUsername() {
+    return Row(
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage('assets/benjeeman.jpg'),
+          radius: 16.0,
+        ),
+        SizedBox(width: 8.0),
+        Text(
+          'Benjeeman',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
   }
 }
