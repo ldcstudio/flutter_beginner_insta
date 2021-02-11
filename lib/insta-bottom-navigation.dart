66599351ed556bf9103b5f4603ea32f93a5454a6
Jensen098 <nickjensen098@gmail.com>
2021-02-11 18:16:47 +1000

    Create insta-bottom-navigation.dart

--- lib/insta-bottom-navigation.dart
+++ lib/insta-bottom-navigation.dart
@@ -3,6 +3,21 @@ import 'package:flutter/material.dart';
 class InstaBottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 24.0,
+        vertical: 16.0,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(Icons.home_filled),
+          Icon(Icons.search),
+          Icon(Icons.add_circle_outline),
+          Icon(Icons.favorite_border),
+          Icon(Icons.person_outline),
+        ],
+      ),
+    );
   }
 }
