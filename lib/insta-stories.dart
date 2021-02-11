0a30c6c11b838043be7ebb024354ff687cc55fe4
Jensen098 <nickjensen098@gmail.com>
2021-02-11 18:01:58 +1000

    Create insta-stories.dart

--- lib/insta-stories.dart
+++ lib/insta-stories.dart
@@ -3,6 +3,65 @@ import 'package:flutter/material.dart';
 class InstaStories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16.0,
+        vertical: 8.0,
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Stories',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Watch All',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildInstaStory(
+                imageAsset: 'assets/benjeeman.jpg',
+                title: 'Your Story',
+              ),
+              _buildInstaStory(
+                imageAsset: 'assets/huseyintopcu.jpg',
+                title: 'huseyintopcu',
+              ),
+              _buildInstaStory(
+                imageAsset: 'assets/botanicalnature.jpg',
+                title: 'botanicalnature',
+              ),
+              _buildInstaStory(
+                imageAsset: 'assets/empproductions.jpg',
+                title: 'empproduct',
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildInstaStory({String imageAsset, String title}) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage(imageAsset),
+            radius: 32.0,
+          ),
+          SizedBox(height: 6.0),
+          Text(title),
+        ],
+      ),
+    );
   }
 }
