26f89a84036dbb1c386d4eb624fead1cc56b16ca
Jensen098 <nickjensen098@gmail.com>
2021-02-11 17:42:56 +1000

    Create insta-app-bar.dart

--- lib/insta-app-bar.dart
+++ lib/insta-app-bar.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
 class InstaAppBar extends StatelessWidget {
+  final double _appBarHeight = 56.0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Container(
+        height: _appBarHeight,
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(Icons.camera_alt),
+            Image.asset('assets/insta_logo_text.png'),
+            Icon(Icons.send)
+          ],
+        ),
+      ),
+    );
   }
 }
